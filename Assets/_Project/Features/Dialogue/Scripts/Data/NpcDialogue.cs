4642b70d5abd47e35838310e8054c378a45efe80
Dmitrii <dshinoda25@gmail.com>
2026-04-24 21:21:54 +0700

    CLP-008-06 -- Changed NpcDialogue

--- Assets/_Project/Features/Dialogue/Scripts/Data/NpcDialogue.cs
+++ Assets/_Project/Features/Dialogue/Scripts/Data/NpcDialogue.cs
@@ -1,22 +1,103 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NpcDialogue : MonoBehaviour, IInteractable
 {
-    [SerializeField] private DialogueData dialogue;
-    [SerializeField] private DialogueType type;
+    public string GetInteractionPrompt() => "Press [E] to talk";
 
-    public string GetInteractionPrompt()
-    {
-        return "Press [E] to talk";
-    }
 
     public bool CanInteract()
     {
-        return dialogue != null && !DialogueManager.Instance.IsActive;
+        return !DialogueManager.Instance.IsActive;
     }
 
     public void Interact()
     {
-        DialogueManager.Instance.StartDialogue(dialogue, type, transform);
+        var events = BubbleDialogue();
+
+        DialogueManager.Instance.StartDialogue(events, DialogueType.RPG, transform);
+    }
+
+    private List<IDialogueEvent> BubbleDialogue()
+    {
+        var endNode = new DialogueNode
+        {
+            text = "Понятно."
+        };
+
+        var goodNode = new DialogueNode
+        {
+            text = "Рад это слышать!",
+            nextNode = endNode
+        };
+
+        var badNode = new DialogueNode
+        {
+            text = "Бывает...",
+            nextNode = endNode
+        };
+
+        var secondQuestion = new DialogueNode
+        {
+            text = "Как у тебя дела?",
+            choices = new List<DialogueChoice>
+            {
+                new DialogueChoice { text = "Хорошо", nextNode = goodNode },
+                new DialogueChoice { text = "Плохо", nextNode = badNode }
+            }
+        };
+
+        var nameResponse = new DialogueNode
+        {
+            text = "Приятно познакомиться!",
+            nextNode = secondQuestion
+        };
+
+        var rudeNode = new DialogueNode
+        {
+            text = "Ну и ладно...",
+            nextNode = secondQuestion
+        };
+
+        var askName = new DialogueNode
+        {
+            text = "Как тебя зовут?",
+            choices = new List<DialogueChoice>
+            {
+                new DialogueChoice { text = "Не твоё дело", nextNode = rudeNode },
+                new DialogueChoice { text = "Меня зовут Игрок", nextNode = nameResponse }
+            }
+        };
+
+        var startNode = new DialogueNode
+        {
+            text = "Привет! Меня зовут NPC.",
+            nextNode = askName
+        };
+
+        return ConvertToEvents(startNode);
+    }
+
+    private List<IDialogueEvent> ConvertToEvents(DialogueNode start)
+    {
+        var events = new List<IDialogueEvent>();
+
+        DialogueNode current = start;
+
+        while (current != null)
+        {
+            events.Add(new TypewriterEvent { text = current.text, speed = 0.05f });
+
+            if (current.choices != null && current.choices.Count > 0)
+            {
+                events.Add(new ChoiceEvent { choices = current.choices });
+
+                break;
+            }
+
+            current = current.nextNode;
+        }
+
+        return events;
     }
 }
