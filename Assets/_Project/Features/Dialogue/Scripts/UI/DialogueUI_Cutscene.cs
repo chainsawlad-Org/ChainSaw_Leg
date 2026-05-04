ebd48c9266811376b230ead22032c731006249f2
Dmitrii <dshinoda25@gmail.com>
2026-05-04 20:55:09 +0700

    CLP-008-013-- Added a system of dialogues for the implementation of cutscenes

--- Assets/_Project/Features/Dialogue/Scripts/UI/DialogueUI_Cutscene.cs
+++ Assets/_Project/Features/Dialogue/Scripts/UI/DialogueUI_Cutscene.cs
@@ -6,9 +6,15 @@ public class DialogueUI_Cutscene : MonoBehaviour
     [SerializeField] private GameObject root;
     [SerializeField] private TextMeshProUGUI text;
 
+    private void Awake()
+    {
+        root.SetActive(false);
+    }
+
     public void ShowRoot()
     {
         root.SetActive(true);
+        Clear();
     }
 
     public void ShowText(string t)
@@ -20,5 +26,11 @@ public class DialogueUI_Cutscene : MonoBehaviour
     public void Hide()
     {
         root.SetActive(false);
+        Clear();
+    }
+
+    private void Clear()
+    {
+        text.text = "";
     }
 }
