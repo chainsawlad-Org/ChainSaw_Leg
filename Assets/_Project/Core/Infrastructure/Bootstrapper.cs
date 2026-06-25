f95d4fb8a16ac8246cd8a415a9f59f34a9fab602
Dmitrii <dshinoda25@gmail.com>
2026-06-25 21:01:56 +0700

    CLP-014-14 -- Updated Bootstrapper

--- Assets/_Project/Core/Infrastructure/Bootstrapper.cs
+++ Assets/_Project/Core/Infrastructure/Bootstrapper.cs
@@ -1,6 +1,23 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using Zenject;
 
 public class Bootstrapper : MonoBehaviour
 {
+    [Inject] private GameStateMachine gameStateMachine;
+    [Inject] private ISceneLoader sceneLoader;
 
+    private async void Start()
+    {
+        await StartGame();
+    }
+
+    private async UniTask StartGame()
+    {
+        await sceneLoader.LoadAdditive("Presistent");
+
+        MainMenuPhase mainMenuPhase = new MainMenuPhase(sceneLoader);
+
+        await gameStateMachine.Push(mainMenuPhase);
+    }
 }
