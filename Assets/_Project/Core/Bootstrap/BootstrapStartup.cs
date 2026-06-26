bbff01242636c53ab0d873cb8ed37bd982913399
Dmitrii <dshinoda25@gmail.com>
2026-06-26 23:53:12 +0700

    CLP-014-19 -- Create BootstrapStartup

---
+++ Assets/_Project/Core/Bootstrap/BootstrapStartup.cs
@@ -0,0 +1,27 @@
+using Cysharp.Threading.Tasks;
+using NUnit.Framework;
+using Zenject;
+
+public class BootstrapStartup : IInitializable
+{
+    private readonly GameStateMachine gameStateMachine;
+    private readonly ISceneLoader sceneLoader;
+
+    public BootstrapStartup(GameStateMachine gameStateMachine, ISceneLoader sceneLoader)
+    {
+        this.gameStateMachine = gameStateMachine;
+        this.sceneLoader = sceneLoader;
+    }
+
+    public void Initialize()
+    {
+        Start().Forget();
+    }
+
+    private async UniTask Start()
+    {
+        await sceneLoader.LoadAdditive("SC_Persistent");
+
+        await gameStateMachine.Push(new MainMenuPhase(sceneLoader));
+    }
+}
