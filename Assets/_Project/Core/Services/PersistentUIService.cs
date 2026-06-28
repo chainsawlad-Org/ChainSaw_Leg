0ad4f6e8bcd274786612e5bd44d505046e2778db
Dmitrii <dshinoda25@gmail.com>
2026-06-28 22:46:20 +0700

    CLP-014-40 -- Change Persistent Scene

---
+++ Assets/_Project/Core/Services/PersistentUIService.cs
@@ -0,0 +1,12 @@
+// using Cysharp.Threading.Tasks;
+
+// public class PersistentUIService
+// {
+//     public UniTask ShowLoading();
+//     public UniTask HideLoading();
+
+//     public UniTask FadeIn();
+//     public UniTask FadeOut();
+
+//     public UniTask ShowPopup(string text);
+// }
