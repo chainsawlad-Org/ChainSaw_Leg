e70fdc6b309a6fa9bdac752a6796f0d9501f4264
Sgifch <mivachi@inbox.ru>
2026-08-10 16:45:58 +0400

    Added inheritance from ApplicationServiceBase.

--- Assets/_Project/Game/Features/Dialogue/Scripts/Data/DialogueDatabaseService.cs
+++ Assets/_Project/Game/Features/Dialogue/Scripts/Data/DialogueDatabaseService.cs
@@ -1,59 +1,19 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System;
-using Newtonsoft.Json.Linq;
 
-public class DialogueDatabaseService
+public class DialogueDatabaseService : ApplicationServiceBase
 {
     public List<DialogueNode> nodes = new List<DialogueNode>();
     public Dictionary<string, DialogueNode> nodesDatabase = new Dictionary<string, DialogueNode>();
+
+    //override Initialize();
 
     public bool isBuilt = false;
 
     public void Build()
     {
-        /*string jsonData = DialogueImporter.Load(nameJson, pathToJson);
 
-        if (jsonData != null)
-        {
-            JArray array = DialogueImporter.Parse(jsonData);
-
-            foreach (JObject obj in array)
-            {
-
-                string id = obj["id"].Value<string>();
-
-                if (Contains(id))
-                {
-                    Debug.LogError("id " + id + " already exists");
-                    break;
-                }
-
-                DialogueNode node = new DialogueNode
-                {
-                    speaker = obj["speaker"].Value<string>(),
-                    text = obj["text"].Value<string>(),
-                    nextId = obj["next_id"].Value<string>(),
-                };
-
-                nodesDatabase.Add(id, node);
-                nodes.Add(node);
-            }
-
-            if (DialogueImporter.Validate(nodesDatabase))
-            {
-                Debug.Log("Dialogue database is importing");
-                isBuilt = true;
-            }
-            else
-            {
-                Debug.LogError("Dialogue database is not importing");
-            }
-        }
-        else
-        {
-            Debug.LogError("File " + nameJson + " is null");
-        }*/
     }
 
     public DialogueNode GetNode(string id)
