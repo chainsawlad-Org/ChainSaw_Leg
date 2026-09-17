0936a047fd6748ea5fc98bddab9b4ba37c30f73e
TUTTIK-FRUTTIK <76999364+TUTTIK-FRUTTIK@users.noreply.github.com>
2026-09-18 02:18:55 +0500

    Fixed setup for debug panel

--- Assets/_Project/Infrastructure/Debug/BaseDebugPage.cs
+++ Assets/_Project/Infrastructure/Debug/BaseDebugPage.cs
@@ -14,9 +14,9 @@ public sealed class BaseDebugPage : DefaultDebugPageBase
 
     public override IEnumerator Initialize()
     {
-        AddSlider(15f, 15f, 30f, "Camera zoom", valueChanged: x => Camera.main.orthographicSize = x);
-        AddSlider(1f, 1f, 3f, "Player size", valueChanged: x => playerTransform.localScale = new Vector3(-x, x, 1f));
-        AddSlider(9f, 9f, 20f, "Player speed", valueChanged: x => playerTransform.GetComponent<PlayerMovement>().SetSpeed(x));
+        AddSlider(Camera.main.orthographicSize, 15f, 30f, "Camera zoom", valueChanged: x => Camera.main.orthographicSize = x);
+        AddSlider(playerTransform.localScale.x, 1f, 3f, "Player size", valueChanged: x => playerTransform.localScale = new Vector3(x, x, 1f));
+        AddSlider(9f, 9f, 30f, "Player speed", valueChanged: x => playerTransform.GetComponent<PlayerMovement>().SetSpeed(x));
 
         yield break;
     }
